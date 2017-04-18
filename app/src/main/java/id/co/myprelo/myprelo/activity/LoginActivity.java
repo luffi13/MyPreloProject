d9fc49d11796bdccbf2e462e27d4d2ed7607ebf6
Luffi Aditya Sandy <luffi.as@gmail.com>
2017-04-18 22:33:27 +0700

    generate activity package

--- app/src/main/java/id/co/myprelo/myprelo/activity/LoginActivity.java
+++ app/src/main/java/id/co/myprelo/myprelo/activity/LoginActivity.java
@@ -1,7 +1,9 @@
-package id.co.myprelo.myprelo;
+package id.co.myprelo.myprelo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import id.co.myprelo.myprelo.R;
 
 public class LoginActivity extends AppCompatActivity {
 
