e09e67d734ddb4c3640859985ed1bdf8ce7c1248
Luffi Aditya Sandy <luffi.as@gmail.com>
2017-04-18 23:16:13 +0700

    create profile activity

---
+++ app/src/main/java/id/co/myprelo/myprelo/activity/ProfileActivity.java
@@ -0,0 +1,15 @@
+package id.co.myprelo.myprelo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.co.myprelo.myprelo.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
